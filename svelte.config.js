44defd3f253522bfe1f19159f22aec28d8943d89
bherbruck <brennen.herbruck@gmail.com>
2021-11-23 23:12:22 -0500

    fix paths for gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
 import preprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-static'
+import { name as packageName } from './package.json'
+
+const ROOT_ROUTE = `/${packageName}`
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,6 +14,10 @@ const config = {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
     adapter: adapter(),
+    paths: {
+      assets: ROOT_ROUTE,
+      base: ROOT_ROUTE,
+    },
   },
 }
 
