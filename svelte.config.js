b655636d6a5080237ec53afc939b21bd18a99837
bherbruck <brennen.herbruck@gmail.com>
2022-03-17 19:10:14 -0400

    remove target

--- svelte.config.js
+++ svelte.config.js
@@ -17,8 +17,6 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
     adapter: adapter(),
     paths,
   },
