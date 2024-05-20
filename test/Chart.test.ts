bdbd08be25eca7ed9b4d39f9230a226b7882d300
bherbruck <brennen.herbruck@gmail.com>
2024-05-20 16:53:10 -0400

    fix: Mock ResizeObserver in Chart.test.ts

--- test/Chart.test.ts
+++ test/Chart.test.ts
@@ -92,6 +92,16 @@ const customTheme = {
   },
 }
 
+// Mock the ResizeObserver
+const ResizeObserverMock = vi.fn(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn(),
+}))
+
+// Stub the global ResizeObserver
+vi.stubGlobal('ResizeObserver', ResizeObserverMock)
+
 describe('Chart Component', () => {
   echarts.use([
     BarChart,
