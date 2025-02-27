02b952608069a249a0ad1b4829fb984e06a76b4d
rxliuli <rxliuli@gmail.com>
2025-02-28 00:10:44 +0800

    fix: fix two-way data binding (#36)

---
+++ tests/Chart.test.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { Chart } from '$lib/svelte-echarts'
+  import type { EChartsType } from 'echarts'
+  import type { ChartProps } from 'svelte-echarts/types'
+  import type { Writable } from 'svelte/store'
+
+  let {
+    chartStore,
+    init,
+    options,
+  }: Pick<ChartProps, 'init' | 'options'> & {
+    chartStore: Writable<EChartsType>
+  } = $props()
+</script>
+
+<Chart {init} {options} bind:chart={$chartStore} />
